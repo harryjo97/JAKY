f3077b7fe512e1a309a0d1710147965a212e2a9a
harryjo97 <50068750+harryjo97@users.noreply.github.com>
2019-05-07 16:52:55 +0900

    Add files via upload

---
+++ JAKY_Timer.js
@@ -0,0 +1,32 @@
+var divClear = document.getElementById("clear");
+
+var T=60000;
+var t=0;
+
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(timer);
+function timer(){
+  var data = google.visualization.arrayToDataTable([
+          ['Time', 'Percentage'],
+          ['Spent',      t],
+          ['Remaining',T-t],
+        ]);
+
+        var options = {
+          title: 'Timer',
+          backgroundColor: 'white',
+          colors: ['white','red'],
+          legend: 'none',
+          pieSliceText: 'none'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+        if(t<=T){t+=100;}
+}
+var clock = setInterval("timer()",100);
+
+function clearTimer(){
+  clearInterval(clock);
+}
